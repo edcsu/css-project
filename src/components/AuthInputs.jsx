9bb992a96e0714b2c9cab8b28e065f0e9634573f
Ssewannonda Keith Edwin <skeith696@gmail.com>
2025-02-06 16:43:44 +0300

    style: finalise migration to tailwind

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -3,12 +3,6 @@ import { styled } from 'styled-components';
 import Button from './Button';
 import CustomInput from './Inputs';
 
-const ControlDiv =  styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 0.5rem;
-  margin-bottom: 1.5rem;
-`
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState('');
   const [enteredPassword, setEnteredPassword] = useState('');
@@ -30,8 +24,8 @@ export default function AuthInputs() {
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
-    <div id="auth-inputs">
-      <ControlDiv>
+    <div id="auth-inputs" className="w-full max-w-sm p-8 mx-auto rounded shadow-md bg-gradient-to-b from-stone-700 to-stone-800">
+      <div className="flex flex-col gap-2 mb-6">
         <CustomInput
           label="Email"
           type="email"
@@ -46,9 +40,9 @@ export default function AuthInputs() {
             handleInputChange('password', event.target.value)
           }
         />
-      </ControlDiv>
-      <div className="actions">
-        <button type="button" className="text-button">
+      </div>
+      <div className="flex justify-end gap-4">
+        <button type="button" className="text-amber-400 hover:text-amber-500 cursor-pointer">
           Create a new account
         </button>
         <Button onClick={handleLogin}>Sign In</Button>
